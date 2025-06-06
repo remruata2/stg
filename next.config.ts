37a4ed42ed4eb73eb76e48642ae288ded8d46e71
Remruata <erp@mzhssp.in>
2025-06-06 14:56:05 +0530

    3001

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    PORT: "3001"
+  },
+  // Ensure basePath is not set if you're not serving from a subdirectory
+  // basePath: "",
 };
 
 export default nextConfig;
